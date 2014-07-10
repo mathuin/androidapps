ddf1c552da8ef1875b311db027de58393bd215c6
Jack Twilley <mathuin@gmail.com>
2014-07-09 19:48:26 -0700

    Added new template and Javascript files.
    The Javascript function that toggles showing or hiding the older
    changes has been added as well as the page that uses the function.

---
+++ static/js/toggle.js
@@ -0,0 +1,11 @@
+function toggle(showHideDiv, switchTextDiv) {
+    var ele = document.getElementById(showHideDiv);
+    var text = document.getElementById(switchTextDiv);
+    if (ele.style.display == "block") {
+	ele.style.display = "none";
+	text.innerHTML = "Display old changes";
+    } else {
+	ele.style.display = "block";
+	text.innerHTML = "Hide old changes";
+    }
+}
